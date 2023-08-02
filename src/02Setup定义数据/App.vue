bcf1ceab7239627431beb0b7c646a277a5887221
HoloBook <zhongerbaohuxiehui@foxmail.com>
2023-08-02 18:35:48 +0800

    renamed:    src/App.vue -> "src/01Setup\344\275\223\351\252\214/App.vue" 	renamed:    src/assets/logo.png -> "src/01Setup\344\275\223\351\252\214/assets/logo.png" 	renamed:    src/hooks/userCounter.js -> "src/01Setup\344\275\223\351\252\214/hooks/userCounter.js" 	new file:   "src/02Setup\345\256\232\344\271\211\346\225\260\346\215\256/App.vue" 	modified:   src/main.js

---
+++ src/02Setup定义数据/App.vue
@@ -0,0 +1,56 @@
+<template>
+    <h2>02 </h2>
+    <p>msgtime:{{ msg }}</p>
+    <!-- <button @click="msgBtn">修改msg</button> -->
+
+    <hr>
+    <h2>name：{{ fullName.name }}</h2>
+    <h2>age: {{ fullName.age }}</h2>
+    <button @click="fullNameBtn"> 修改fullName</button>
+
+    <br>
+
+    <h2>当前计数：{{ number }}</h2>
+    <button @click="AddNumberBtn">+1</button>
+</template>
+
+<script>
+import { reactive , ref } from 'vue'
+
+// import { ref } from 'vue'
+
+export default {
+    setup() {
+        let msg = "msg111"
+        const number = ref(1)
+        //定义响应式数据   用reactive   reactive定义复杂数据
+        //简单数据用ref   也可以定义复杂类型数据
+        let fullName = reactive({
+            name: "yjh",
+            age: 18
+
+        })
+        // const msgBtn = () => {
+        //     msg.value = "newnewnewnewe"
+        // }
+        const fullNameBtn = () => {
+                fullName.name="newyjh",
+                fullName.age="188"
+        }
+        const AddNumberBtn = () => {
+            number.value++
+        }
+        return {
+            msg,
+            fullName,
+            fullNameBtn,
+            number,
+            AddNumberBtn
+            // msgBtn
+        }
+    }
+
+}
+</script>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+
+// import App from './01Setup体验/App.vue'
+
+import App from './02Setup定义数据/App.vue'
 
 createApp(App).mount('#app')
