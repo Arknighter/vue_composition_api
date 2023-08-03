7215c9f0a7197a850cef3a27528badeaf1dce3c5
HoloBook <zhongerbaohuxiehui@foxmail.com>
2023-08-03 22:02:27 +0800

    new file:   "src/06Setup\344\270\255ref\345\274\225\345\205\245\345\205\203\347\264\240/App.vue" 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import { createApp } from 'vue'
 // import App from './02Setup定义数据/App.vue'
 // import App from './03Setup的其他函数/App.vue'
 // import App from './04Setip的toRefs/App.vue'
-import App from './05Setup的computed/App.vue'
+// import App from './05Setup的computed/App.vue'
+import App from './06Setup中ref引入元素/App.vue'
 
 // import App from './02Setup定义数据/AppCat.vue'
 createApp(App).mount('#app')
