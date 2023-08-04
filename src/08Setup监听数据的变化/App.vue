b7cca61a80b72f36bdcbb8be274baf6e2f598f8f
HoloBook <zhongerbaohuxiehui@foxmail.com>
2023-08-04 20:36:15 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   "src/08Setup\347\233\221\345\220\254\346\225\260\346\215\256\347\232\204\345\217\230\345\214\226/App.vue" 	modified:   src/main.js

--- src/08Setup监听数据的变化/App.vue
+++ src/08Setup监听数据的变化/App.vue
@@ -1,26 +1,61 @@
 <template>
     <h2>App 监听</h2>
     <button @click="msgBtn"> 修改数据</button>
+
+    <el-button>我是 ElButton</el-button>
+
+    <el-carousel :interval="5000" arrow="always">
+    <el-carousel-item v-for="item in 4" :key="item">
+      <h3 text="2xl" justify="center">{{ item }}</h3>
+    </el-carousel-item>
+    </el-carousel>
+
+    <div class="flex flex-wrap items-center">
+    <el-dropdown>
+      <el-button type="primary">
+        Dropdown List<el-icon class="el-icon--right"><arrow-down /></el-icon>
+      </el-button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item>Action 2</el-dropdown-item>
+          <el-dropdown-item>Action 3</el-dropdown-item>
+          <el-dropdown-item>Action 4</el-dropdown-item>
+          <el-dropdown-item>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+    </div>
 </template>
+
 <script>
 import { ref, watch } from 'vue'
+import { ArrowDown } from '@element-plus/icons-vue'
 
 export default {
+    components:{
+        ArrowDown
+    },
     setup() {
+
         const msg = ref("hello")
 
         const msgBtn = () => {
-            msg.value="new hello"
+            msg.value = "new hello"
         }
 
-        watch(msg,(newValue,oldeValue) => {
-            console.log("newmsg="+newValue,"old="+oldeValue);
+        watch(msg, (newValue, oldeValue) => {
+            console.log("newmsg=" + newValue, "old=" + oldeValue);
         })
 
-
+        const handleClick = () => {
+            // eslint-disable-next-line no-alert
+            alert('button click')
+        }
         return {
             msg,
-            msgBtn
+            msgBtn,
+            handleClick
         }
     }
 
@@ -28,4 +63,30 @@ export default {
 
 
 </script>
-<style scoped></style>
+<style scoped>
+.el-carousel__item h3 {
+  color: #475669;
+  opacity: 0.75;
+  line-height: 300px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+
+.example-showcase .el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 // import App from './01Setup体验/App.vue'
-
 // import App from './02Setup定义数据/App.vue'
 // import App from './03Setup的其他函数/App.vue'
 // import App from './04Setip的toRefs/App.vue'
@@ -11,4 +12,11 @@ import { createApp } from 'vue'
 import App from './08Setup监听数据的变化/App.vue'
 
 // import App from './02Setup定义数据/AppCat.vue'
-createApp(App).mount('#app')
+
+// createApp(App).mount('#app')
+
+
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app');
